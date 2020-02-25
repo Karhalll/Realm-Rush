b66a233fcbbf4afdec6acf2a8fc07b0aa464562e
Karhal <mar.fucik@gmail.com>
2020-02-25 22:03:41 +0100

    Initial commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = null;
+
+    private void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        print("Starting patrol");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting: " + waypoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Starting patrol");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
