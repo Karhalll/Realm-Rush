4f4a1fbb4a7f826ececd29e6cb0dd51ea001ceba
Karhal <mar.fucik@gmail.com>
2020-02-26 21:32:47 +0100

    Finished Pathfinding
    + creating map
    + turret turning to enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private void Start()
+    {
+        Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
+        var path = pathfinder.GetPath();
+        StartCoroutine(FollowPath(path));
+    }
+
+    private IEnumerator FollowPath(List<Waypoint> path)
+    {
+        print("Starting patrol");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan = null;
+    [SerializeField] Transform targetEnemy = null;
+
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
